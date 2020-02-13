d53c91f9413194f4c523cfee4f14249a8c20797b
K. Raphael <raphalogou@gmail.com>
2020-02-14 00:06:59 +0100

    Updated gitignore

--- main.dart
+++ main.dart
@@ -2,12 +2,12 @@ import 'src/lox.dart';
 
 main(List<String> argv) {
 	Lox lox = new Lox();
-	
+
 	if (argv.length > 1) {
-		print("Usage: dlox [script]");
+    	print("Usage: dlox [script]");
 	} else if (argv.length == 1) {
-		lox.runFile(argv[0]);
-  	} else {
+    	lox.runFile(argv[0]);
+	} else {
 		lox.prompt();
-  	}
+	}
 }
